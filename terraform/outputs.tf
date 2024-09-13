1a00e483aa7fd6dde6dd035c2e210f53321f1a38
Steven Cassamajor <contributor@networkaxis.org>
2024-09-13 00:03:00 -0400

    Correct shape for local variable, add ami_id to output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
-output "vmimport_role_arn" {
-  description = "The ARN of the VMimport role."
+output "iam_role_arn" {
+  description = "ARN of the IAM role used for VM Import"
   value       = module.image_uploader.iam_role_arn
+}
+
+output "ami_id" {
+  description = "ID of the imported AMI"
+  value       = module.image_uploader.ami_id
 }
